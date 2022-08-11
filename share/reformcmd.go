e90f99377a0e733a688975d56cc4c77b003ed9bb
mkthun <mkthun@protonmail.com>
2022-08-11 10:51:05 +0100

    Add functions, fixed errors, typos

--- share/reformcmd.go
+++ share/reformcmd.go
@@ -12,6 +12,7 @@ import (
 // This module will be executed before argparsing of command line arguments to evade IDS system
 // In conjunction with how ninja-gencmd transpires
 
+
 //First Check any encoding by characteristics
 func checkEncoding(s string) {
         // Check if its Gzip
@@ -47,24 +48,24 @@ func decodeBase64(s string) (result string, err error) {
 }
 
 // Remove extra spaces
-func removeExcessWS(s string) {
+func removeExcessWS(s string) string {
         originalSize := len(s)-1
         wsCount := strings.Count(s,' ')
         resizeSize := originalSize - (wsCount/2)
+	builder := sitnrgs.Builder{}
         //if checkEven(resizeSize) != true {
 
-        //TODO goHelp functions str slice making
-        slice := strings.SplitN(s, ' ', -1)
-        for i, tok := range slice {
-
-                if i > 0  && (string.Contains(slice[i], ' ' && string.Contains(slice[i-1], ' ') {
-                        //Skip
-                } else {
-                //Add to resultSlice
-                }
+	result := initStrArray(resizeSize)
+	for i := 0; i <=originalSize; i++ {
+		if i > 0  && (string.Contains(slice[i], ' ' && string.Contains(slice[i-1], ' ') || !string.Contains(slice[1], ' ') { 
+			builder.WriteString(slice[i])
+               }
         }
-        strings.Join(
-        return 
+        return builder.String() 
 }
 
 // Change case to lower case
+func convLowerCase(s string) string {
+	return strings.ToLower(s)
+}
+
