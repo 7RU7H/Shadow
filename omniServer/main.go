885856a84208e1e1b9475784a35c40ede1dd77cb
truth <mkthun@protonmail.com>
2023-07-21 23:03:40 +0100

    Fixing Ctrl Flow; added more logging

--- omniServer/main.go
+++ omniServer/main.go
@@ -1,10 +1,13 @@
-package omniServer 
+package omniServer
 
 import (
 	"fmt"
 	"io/ioutil"
 	"log"
 	"net/http"
+	"os"
+	"strconv"
+	"strings"
 	"time"
 	//cli.go
 )
@@ -15,71 +18,70 @@ import (
 // https://www.digitalocean.com/community/tutorials/how-to-make-an-http-server-in-go
 // https://www.digitalocean.com/community/tutorials/how-to-use-contexts-in-go
 
-
 // Upload file - filename
 func uploadFileHandler(w http.ResponseWriter, r *http.Request) error {
 
-    // Parse our multipart form, 10 << 20 specifies a maximum
-    // upload of 10 MB files.
-    //r.ParseMultipartForm(10 << 20)
+	// Parse our multipart form, 10 << 20 specifies a maximum
+	// upload of 10 MB files.
+	//r.ParseMultipartForm(10 << 20)
 
 	// Get filename from body of r.Body
-	
+
 	// FormFile returns the first file for the given key `myFile`
-    // it also returns the FileHeader so we can get the Filename,
-    // the Header and the size of the file
+	// it also returns the FileHeader so we can get the Filename,
+	// the Header and the size of the file
 
-    log.Printf("/upload/%s - Upload requested by ...", handler.Filename)
-    file, handler, err := r.FormFile()
-    if err != nil {
+	log.Printf("/upload/%s - Upload requested by ...", handler.Filename)
+	file, handler, err := r.FormFile()
+	if err != nil {
 		// Error retrieving file of filename
-        return err
-    }
+		return err
+	}
 	startTime := time.Now()
-    defer file.Close()
-	//log.Print("",  ) File upload request success 
-	//log.Print("",  ) File upload INFO: 
-    log.Printf("Uploaded File: %+v\n", handler.Filename)
-    lof.Printf("File Size: %+v\n", handler.Size)
-    log.Printf("MIME Header: %+v\n", handler.Header)
-	//log.Print("",  ) File upload request success 
+	defer file.Close()
+	//log.Print("",  ) File upload request success
+	//log.Print("",  ) File upload INFO:
+	log.Printf("Uploaded File: %+v\n", handler.Filename)
+	lof.Printf("File Size: %+v\n", handler.Size)
+	log.Printf("MIME Header: %+v\n", handler.Header)
+	//log.Print("",  ) File upload request success
 
-    // Create a temporary file within our temp-images directory that conforms to a naming scheme
-    tempFile, err := ioutil.TempFile(tmpUploadDir, "tmp-")
-    if err != nil {
-        // Error creating temporary file
+	// Create a temporary file within our temp-images directory that conforms to a naming scheme
+	tempFile, err := ioutil.TempFile(tmpUploadDir, "tmp-")
+	if err != nil {
+		// Error creating temporary file
 		return err
-    }
-    defer tempFile.Close()
+	}
+	defer tempFile.Close()
 
-    // read all of the contents of our uploaded file into a byte array
-    fileBytes, err := ioutil.ReadAll(file)
-    if err != nil {
-        // Failed to read file being uploaded to byte array
+	// read all of the contents of our uploaded file into a byte array
+	fileBytes, err := ioutil.ReadAll(file)
+	if err != nil {
+		// Failed to read file being uploaded to byte array
 		return err
-    }
-    // write this byte array to our temporary file
-    tempFile.Write(fileBytes)
+	}
+	// write this byte array to our temporary file
+	tempFile.Write(fileBytes)
 	endTime := time.Now()
-    //Return that we have successfully uploaded our file!
+	//Return that we have successfully uploaded our file!
 	log.Printf("Successfully Uploaded File - %s \n", handler.Filename)
-    fmt.Fprintf(w, "Successfully Uploaded File - %s \n", handler.Filename)
+	fmt.Fprintf(w, "Successfully Uploaded File - %s \n", handler.Filename)
 	return nil
 }
 
 // Download file - filename
 func downloadFileHandler(w http.ResponseWriter, r *http.Request) error {
-	
+
 	// client := Headers - IP User-Agent
-	// requestedFileToDownload := 
-	
+	// requestedFileToDownload :=
+
 	if !checkFileExists(requestedFileToDownload) {
-        w.WriteHeader(404)
-        w.Write([]byte("404\n"))
+		w.WriteHeader(404)
+		w.Write([]byte("404\n"))
 		log.Fatal("Failed to Download file: %s - requested by: %s, %s", requestedFiletoDownload, clientIP, clientUA)
 		// Fail to download file error
 		return err
-    } else {  
+	} else {
 		startTime := time.Now()
 		log.Printf("Downloading file at:  %s - requested by: %s, %s", requestedFiletoDownload, clientIP, clientUA)
 	}
@@ -97,7 +99,7 @@ func saveReqBodyFileHandler(w http.ResponseWriter, r *http.Request) error {
 		return err
 	} else {
 		startTime := time.Now()
-		// filename  
+		// filename
 		// create file
 		// write to file
 		// close file
@@ -108,19 +110,19 @@ func saveReqBodyFileHandler(w http.ResponseWriter, r *http.Request) error {
 
 func main() {
 	// Testing values
-	interface := "eth0"
+	//interface := "eth0"
 	vhost := "testwebserver.nvm"
 	isTLS := false
 	customCert := ""
-	listeningPort := 443 
+	listeningPort := 443
 	ipAddress := "127.0.0.1"
 	tlsCert := "/path/to/cert"
 	uploadPath := "/path/to/upload"
 	serverCertPath := "/path/to/cert"
-	serverKeyPath :=  "/path/to/cert"
+	serverKeyPath := "/path/to/cert"
 
 	appStartTime := time.Now()
-	// CLI 
+	// CLI
 	//banner := cli.Banner()
 	//fmt.Printf("\n%s\n", banner)
 	var userDefinedServerKeyPath string
@@ -129,73 +131,84 @@ func main() {
 	// 0: Default 30
 	// 1: Randomised
 	// 2: Customised
-	var certDaysSettings int 
-	// If 2 requires != 0, 
-	var	userDefinedCertExpiryDays int
-	
-	var certExpiryDaysSeed string
-	var certExpiryDaysRangeLowerBound int 
-	var certExpiryDaysRangeUpperBound int
-	
-	var certExpiryDaysRand int
+	var certDaysSettings int
+	// If 2 requires != 0,
+	var userDefinedCertExpiryDays int
 
+	var certExpiryDaysSeed string
+	var certExpiryDaysRangeLowerBound int
+	var certExpiryDaysRangeUpperBound int
+
+	var certExpiryDaysRand int
 
 	// MetaHander - to create, run, close servers - isTLS, vhost, interface, listeningPort, ipAddress
 	// Mux is a multiplexer to handle routes
 	mux := http.NewServeMux()
 
 	// Make TLS or HTTP server
-		// 
-		// TLS
+	// HTTP
+	if !isTLS {
 
-	// Default - host a page
-	// Host main page
-
-	// Setup routes
-	
-	
-	mux.HandleFunc("/upload", uploadFileHandler())
-	mux.HandleFunc("/download", downloadFileHandler()))
-	mux.HandleFunc("/saveReqBody", saveReqBodyFileHandler())
-
-	
-	// 
-	// ListenAndServer either TLS or HTTP server
-		// HTTP	
-
+	} else {
 		// TLS
 		if customCert != "" {
 			serverCertPath = cli.UserDefinedServerCertPath
 			serverKeyPath = cli.UserDefinedServerKeyPath
+			log.Printf("Custom TLS Certificate used at: %s", serverCertPath)
+			log.Printf("Custom TLS Key used at: %s", serverKeyPath)
 		} else {
 			switch certDaysSetting {
-			case: 0 // Default 30
-				tls.CreateTLSCertKeyPair(30)
-			case: 1 // randomised days
-				tls.CreateTLSCertKeyPair()
-			case: 2 // customised days
-				tls.CreateTLSCertKeyPair(userDefinedCertExpiryDays)
+			case 0: // Default 30
+				err := tls.CreateTLSCertKeyPair(30)
+				//
+				log.Printf("TLS Certificate created with expiry of days: 30")
+			case 1: // randomised days
+				//
+				// ADD feature
+				randomExpiryDays := 0
+				err := tls.CreateTLSCertKeyPair(randomExpiryDays)
+				//
+				log.Printf("TLS Certificate created with expiry of days: %d", randomExpiryDays)
+			case 2: // customised days
+				err := tls.CreateTLSCertKeyPair(userDefinedCertExpiryDays)
+				//
+				log.Printf("TLS Certificate created with expiry of days: %d", userDefinedCertExpiryDays)
 			default:
 				// error
 
 			}
 		}
+	}
 
-		// If TLS server TLS certificate
+	// Default - host a page
+	// Host main page
+
+	// Setup routes
+	mux.HandleFunc("/upload", uploadFileHandler())
+	mux.HandleFunc("/download", downloadFileHandler())
+	mux.HandleFunc("/saveReqBody", saveReqBodyFileHandler())
+
+	//
+	// ListenAndServer either HTTP or HTTPS server
+	// HTTP
+	if !isTLS {
+
+	} else {
+		// If HTTPS server
+		//serverStartTime := time.Now()
 		err := http.ListenAndServeTLS(":443", serverCertPath, serverKeyPath, nil)
-    	if err != nil {
-      	  log.Fatal("ListenAndServe: ", err)
-    	}
-
-
+		if err != nil {
+			log.Fatal("ListenAndServe Failed at %s -", time.Now(), err)
+		}
+	}
 
 	// CloseServer
 
 	// CloseApplication
 	appTerminateTime := time.Now()
 	// totalRuntime
-	fmt.Printf("Application started: %s - Terminated: %s - Runtime: %s", appStartTime, appTerminateTime, totalRuntime)
-
+	log.Printf("Application started: %s - Terminated: %s - Runtime: %s", appStartTime, appTerminateTime, totalRuntime)
+	fmt.Fprintf("Application started: %s - Terminated: %s - Runtime: %s", appStartTime, appTerminateTime, totalRuntime)
 }
 
 func convPortNumber(portNumber int) string {
@@ -208,15 +221,14 @@ func convPortNumber(portNumber int) string {
 }
 
 func checkFileExists(path string) (bool error) {
-        _, err := os.Stat(path)
-        if err == nil {
-                log.Fatal(err)
-                return false, err
-        }
-        if os.IsNotExist(err) {
-                log.Fatal("File path does not exist")
-                return false, err
-        }
-        return true, nil
+	_, err := os.Stat(path)
+	if err == nil {
+		log.Fatal(err)
+		return false, err
+	}
+	if os.IsNotExist(err) {
+		log.Fatal("File path does not exist")
+		return false, err
+	}
+	return true, nil
 }
-
